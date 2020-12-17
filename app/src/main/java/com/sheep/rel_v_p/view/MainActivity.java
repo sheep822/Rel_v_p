2fe04e585fc7f5071ca7ee3ab7976d180167ef1d
sheep822 <3090280142@qq.com>
2020-12-17 18:52:22 +0800

    first

---
+++ app/src/main/java/com/sheep/rel_v_p/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sheep.rel_v_p.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sheep.rel_v_p.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
