b7ca2d24f6cb5cc1906660490ef4faa60cf2d011
sheep822 <3090280142@qq.com>
2020-12-17 20:44:25 +0800

    new3

--- two/src/main/java/com/sheep/two/utils/net/URLConstant.java
+++ two/src/main/java/com/sheep/two/utils/net/URLConstant.java
@@ -1,6 +1,6 @@
 package com.sheep.two.utils.net;
 
 public class URLConstant {
-    String BASEURL="http://cdwan.cn:7000/exam2003/";
-    String NEWSLIST=BASEURL+"anewslist.json";
+    public static String BASEURL = "http://cdwan.cn:7000/exam2003/";
+    public static String NEWSLIST = BASEURL + "anewslist.json";
 }
