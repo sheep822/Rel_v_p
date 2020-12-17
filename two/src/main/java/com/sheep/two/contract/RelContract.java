5339a7697f3802384fa11a9909af5a8ba0b7f80f
sheep822 <3090280142@qq.com>
2020-12-17 20:16:58 +0800

    new2

---
+++ two/src/main/java/com/sheep/two/contract/RelContract.java
@@ -0,0 +1,7 @@
+package com.sheep.two.contract;
+
+public class RelContract {
+    public interface RelModel<B>{
+        void getData();
+    }
+}
---
+++ two/src/main/java/com/sheep/two/utils/net/URLConstant.java
@@ -0,0 +1,6 @@
+package com.sheep.two.utils.net;
+
+public class URLConstant {
+    String BASEURL="http://cdwan.cn:7000/exam2003/";
+    String NEWSLIST=BASEURL+"anewslist.json";
+}
